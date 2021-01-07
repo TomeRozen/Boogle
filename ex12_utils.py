55f62dfff458ea33743750aad5d503e459e39fb6
Tomer Rozenstine <tomer151@gmail.com>
2021-01-07 19:40:24 +0200

    added tests and is_valid_path func

--- ex12_utils.py
+++ ex12_utils.py
@@ -1,3 +1,7 @@
+from typing import List, Tuple, Dict
+MIN_COORD = 0
+MAX_COORD = 3
+
 def load_words_dict(file_path):
     words_dict = {}
     with open(file_path) as file:
@@ -5,10 +9,26 @@ def load_words_dict(file_path):
             words_dict[line.strip()] = True
     return words_dict
 
-print(load_words_dict("simple_words.txt"))
 
-def is_valid_path(board, path, words):
-    pass
+def is_valid_path(board: List[List[str]], path: List, words: Dict):
+    word = ""
+    for index, location in enumerate(path):
+        if path.count(location) > 1:
+            return
+        for ind, coord in enumerate(location):
+            if coord > MAX_COORD or coord < MIN_COORD:
+                return
+            if index > 0:
+                if coord - path[index-1][ind] > 1 or \
+                        coord - path[index-1][ind] < -1:
+                    return
+
+        letter = board[location[0]][location[1]]
+        word += letter
+    if word.upper() in words:
+        return word
+
+
 
 
 def find_length_n_words(n, board, words):
--- test_ex12.py
+++ test_ex12.py
@@ -1,6 +1,23 @@
 from ex12_utils import *
 
-def test_load_words_dict()
-    dict_words = load_words_dict("simple_words.txt")
-    assert type(dict_words) == dict
-    assert a
+TRY_BOARD = [['N', 'I', 'P', 'P'],
+             ['C', 'E', 'O', 'F'],
+             ['W', 'S', 'X', 'M'],
+             ['A', 'U', 'L', 'B']]
+
+SMALL_DICT = load_words_dict("simple_words.txt")
+
+def test_load_words_dict():
+    assert type(SMALL_DICT) == dict
+    assert SMALL_DICT['ACCOASTED'] is True
+
+def test_is_valid_path():
+    assert is_valid_path(TRY_BOARD, [(0,0), (0,1), (1,0), (1,1)], SMALL_DICT) == "NICE"
+    assert is_valid_path(TRY_BOARD, [(0,0), (3,0), (3,2), (3,1)], SMALL_DICT) is None
+    assert is_valid_path(TRY_BOARD, [(0,0), (0,0), (1,1), (1,2)], SMALL_DICT) is None
+    assert is_valid_path(TRY_BOARD, [(0,0), (0,1), (1,1), (0,1)], SMALL_DICT) is None
+    assert is_valid_path(TRY_BOARD, [(0,0), (1,1), (2,2), (3,3)], SMALL_DICT) is None
+    assert is_valid_path(TRY_BOARD, [(0,0), (0,1), (0,2), (0,3), (0,4)], SMALL_DICT) is None
+
+
+
