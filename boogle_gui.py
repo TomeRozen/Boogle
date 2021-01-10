0b9e7e81008e9efdf1ad0e29dc9a02d43269ef63
Tomer Rozenstine <tomer151@gmail.com>
2021-01-10 09:55:35 +0200

    added countdown timer, started working on the GUI, and united this part on the main file

--- boogle_gui.py
+++ boogle_gui.py
@@ -1,3 +1,38 @@
+import tkinter as tki
+
 class BoogleGUI:
     def __init__(self):
-        pass
+        root = tki.Tk()
+        root.title("Boogle!")
+
+        self._main_window = root
+        self._outer_frame = tki.Frame(root, bg="gray35",
+                                      highlightbackground="gray26",
+                                      highlightthickness=3)
+        self._display_label = tki.Label(self._outer_frame, width=25)
+        self._right_frame = tki.Frame(self._outer_frame, bg="gray35")
+        self._countdown_label = tki.Label(self._right_frame, text="3:00:00",
+                                          bg="SkyBlue3",
+                                          highlightbackground="gray17",
+                                          highlightthickness=1)
+        self._start_button = tki.Button(self._outer_frame, text="Start",
+                                       font=("Courier", 20), width=11,
+                                       bg="green3")
+        self.pack()
+
+    def pack(self):
+        self._outer_frame.pack(side=tki.TOP, fill=tki.Y, expand=True)
+        self._display_label.pack(side=tki.TOP, fill=tki.BOTH)
+        self._right_frame.pack(side=tki.RIGHT, fill=tki.BOTH, expand=True)
+        self._countdown_label.pack(side=tki.TOP)
+        self._start_button.pack(side=tki.BOTTOM)
+
+    def run(self):
+        self._main_window.mainloop()
+
+    def get_start_button(self):
+        return self._start_button
+
+if __name__ == '__main__':
+    bg = BoogleGUI()
+    bg.run()
--- boogle_model.py
+++ boogle_model.py
@@ -1,3 +1,25 @@
+from ex12_utils import *
+from datetime import datetime, timedelta
 class BoogleModel:
     def __init__(self):
         pass
+
+    def add_letter(self, word, letter):
+        new_word = word + letter
+        return new_word
+
+    def submit_word(self, board, path, words):
+        word = is_valid_path(board, path, words)
+        if word:
+            pass
+
+    def legal_locations(self, location):
+        list_of_locations = []
+        # for
+
+    def calc_end_time(self):
+        return datetime.now() + timedelta(minutes=3)
+
+    def get_countdown(self, end_time):
+        left_time = end_time - datetime.now()
+        return str(left_time)[3:7]+":"+str(left_time)[8:10]
